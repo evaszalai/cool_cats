9154fee83dbe2c680f259bc970c5ea31ea022457
Nguyen Bence <bence325@gmail.com>
2021-03-02 12:26:21 +0100

    feat: added displaySubPrice async function

--- src/main/webapp/static/js/shopping-cart.js
+++ src/main/webapp/static/js/shopping-cart.js
@@ -1,10 +1,29 @@
-function displayTotalPrice() {
-    let totalPrice = 0;
-    let subPrices = document.querySelectorAll('[data-subprice]');
+const shoppingCart = {
 
-    for (let subPrice of subPrices) {
-        totalPrice += parseInt(subPrice.innerHTML);
+    init: function () {
+        this.displaySubPrice().then(() => this.displayTotalPrice());
+    },
+
+    displayTotalPrice: function () {
+        let totalPrice = 0;
+        let subPrices = document.querySelectorAll('[data-subprice]');
+
+        for (let subPrice of subPrices) {
+            totalPrice += parseInt(subPrice.innerHTML);
+        }
+
+        document.querySelector('.price').innerHTML = totalPrice.toString() + '$';
+    },
+
+    displaySubPrice: async function () {
+        let shoppingCartTable = document.querySelector('.shopping-cart-table');
+
+        for (let row of shoppingCartTable.rows) {
+            let unitPrice = parseInt(document.querySelector('[data-unitprice]').innerHTML);
+            let quantity = document.querySelector('.quantity').value;
+            let subPrice = document.querySelector('[data-subprice]');
+
+            subPrice.innerHTML = (unitPrice * quantity).toString() + '$';
+        }
     }
-
-    document.querySelector('.price').innerHTML = totalPrice.toString() + '$';
 }
