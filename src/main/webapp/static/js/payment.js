a608a949464fde9824f0a8ab8aab6a47402b9fe3
Nguyen Bence <bence325@gmail.com>
2021-03-03 13:47:17 +0100

    feat: payment credentials are changing depending on payment method, added payment.js for it

---
+++ src/main/webapp/static/js/payment.js
@@ -0,0 +1,83 @@
+const payment = {
+
+    changePaymentCredentials: function () {
+        let paymentMethod = document.querySelector('#CreditCardType').value;
+        let paymentCredentials = document.querySelector('#payment-credentials');
+
+        while (paymentCredentials.firstChild) {
+            paymentCredentials.removeChild(paymentCredentials.firstChild);
+        }
+
+        if (paymentMethod === 'credit-card') {
+            payment.addCreditCardCredentials(paymentCredentials);
+        } else {
+            payment.addPayPalCredentials(paymentCredentials);
+        }
+    },
+
+    addCreditCardCredentials: function (paymentCredentials) {
+        let creditCardCredentials =
+            '<div class="form-group">\n' +
+            '                <div class="col-md-12"><strong>Credit Card Number:</strong></div>\n' +
+            '                <div class="col-md-12"><input type="text" class="form-control" name="credit_card_number" value=""/></div>\n' +
+            '            </div>\n' +
+            '            <div class="form-group">\n' +
+            '                <div class="col-md-12"><strong>Card CVV:</strong></div>\n' +
+            '                <div class="col-md-12"><input type="text" class="form-control" name="credit_card_code" value=""/></div>\n' +
+            '            </div>\n' +
+            '            <div class="form-group">\n' +
+            '                <div class="col-md-12">\n' +
+            '                    <strong>Expiration Date</strong>\n' +
+            '                </div>\n' +
+            '                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">\n' +
+            '                    <select class="form-control" name="">\n' +
+            '                        <option value="">Month</option>\n' +
+            '                        <option value="01">01</option>\n' +
+            '                        <option value="02">02</option>\n' +
+            '                        <option value="03">03</option>\n' +
+            '                        <option value="04">04</option>\n' +
+            '                        <option value="05">05</option>\n' +
+            '                        <option value="06">06</option>\n' +
+            '                        <option value="07">07</option>\n' +
+            '                        <option value="08">08</option>\n' +
+            '                        <option value="09">09</option>\n' +
+            '                        <option value="10">10</option>\n' +
+            '                        <option value="11">11</option>\n' +
+            '                        <option value="12">12</option>\n' +
+            '                    </select>\n' +
+            '                </div>\n' +
+            '                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">\n' +
+            '                    <select class="form-control" name="">\n' +
+            '                        <option value="">Year</option>\n' +
+            '                        <option value="2015">2015</option>\n' +
+            '                        <option value="2016">2016</option>\n' +
+            '                        <option value="2017">2017</option>\n' +
+            '                        <option value="2018">2018</option>\n' +
+            '                        <option value="2019">2019</option>\n' +
+            '                        <option value="2020">2020</option>\n' +
+            '                        <option value="2021">2021</option>\n' +
+            '                        <option value="2022">2022</option>\n' +
+            '                        <option value="2023">2023</option>\n' +
+            '                        <option value="2024">2024</option>\n' +
+            '                        <option value="2025">2025</option>\n' +
+            '                    </select>\n' +
+            '                </div>\n' +
+            '            </div>'
+
+        paymentCredentials.insertAdjacentHTML('beforeend', creditCardCredentials);
+    },
+
+    addPayPalCredentials: function (paymentCredentials) {
+        let payPalCredentials =
+            '<div class="form-group">\n' +
+            '                <div class="col-md-12"><strong>Username:</strong></div>\n' +
+            '                <div class="col-md-12"><input type="text" class="form-control" name="username" value=""/></div>\n' +
+            '            </div>\n' +
+            '            <div class="form-group">\n' +
+            '                <div class="col-md-12"><strong>Password:</strong></div>\n' +
+            '                <div class="col-md-12"><input type="password" class="form-control" name="password" value=""/></div>\n' +
+            '</div>'
+
+        paymentCredentials.insertAdjacentHTML('beforeend', payPalCredentials);
+    }
+}
