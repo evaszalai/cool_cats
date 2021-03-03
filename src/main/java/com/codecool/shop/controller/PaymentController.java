6cad63a9c6d09a4b89e51416da1d1868537a82a5
Nguyen Bence <bence325@gmail.com>
2021-03-03 11:46:31 +0100

    feat: added PaymentController, payment button in checkout.html redirects to payment page

---
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/payment"})
+public class PaymentController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("products", productDataStore.getAll());
+
+        engine.process("payment.html", context, resp.getWriter());
+    }
+
+}
