26c02b6bb1ae8bd12164e6666e4b62e5f8797b7a
kisstamas86 <kisstamas861023@gmail.com>
2021-03-03 18:13:35 +0100

    fix addtocart

--- src/main/java/com/codecool/shop/controller/AddToCart.java
+++ src/main/java/com/codecool/shop/controller/AddToCart.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.implementation.ProductId;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.ProductInCart;
+import com.codecool.shop.model.Product;
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
@@ -10,20 +13,63 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.util.*;
+
 
 @WebServlet(urlPatterns = {"/cart"})
-public class AddToCart extends HttpServlet {
+public class CartController extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductId obj = new Gson().fromJson(req.getReader(), ProductId.class);
+        ProductInCart productId = new Gson().fromJson(req.getReader(), ProductInCart.class);
 
         HttpSession session = req.getSession();
+        HashMap<Integer, Integer> productsInCart;
+        if (session.getAttribute("productsInCart") == null) {
+            productsInCart = new HashMap<Integer, Integer>();
+        } else {
+            productsInCart = (HashMap<Integer, Integer>) session.getAttribute("productsInCart");
+        }
+        if (productsInCart.size() == 0) {
+            productsInCart.put(productId.getId(), 1);
+        } else {
+            if (!productsInCart.containsKey(productId.getId())) {
+                productsInCart.put(productId.getId(), 1);
+            } else {
+                int newQuantity = productsInCart.get(productId.getId()) + 1;
+                productsInCart.put(productId.getId(), newQuantity);
+            }
+        }
+        session.setAttribute("productsInCart", productsInCart);
 
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+
+        out.println("[{\"answer\": \"OK!\"}]");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(req.getParameter("id"));
+        HttpSession session = req.getSession();
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        List<Product> productsInCart = new ArrayList<Product>();
+        HashMap<Integer, Integer> productsIdAndQuantity = (HashMap<Integer, Integer>) session.getAttribute("productsInCart");
+        for (Map.Entry<Integer, Integer> entry : productsIdAndQuantity.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            Product product = productDataStore.find(key);
+            product.setQuantity(value);
+            productsInCart.add(product);
+        }
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        Gson gson = new Gson();
+        out.println(gson.toJson(productsInCart));
     }
 }
