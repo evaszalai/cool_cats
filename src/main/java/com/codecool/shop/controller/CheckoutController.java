baee26497214e15767b9471209a6c1f9b88891f7
Nguyen Bence <bence325@gmail.com>
2021-03-03 11:31:33 +0100

    feat: added checkout.html and CheckoutController java class

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/checkout"})
+public class CheckoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("products", productDataStore.getAll());
+
+        engine.process("product/checkout.html", context, resp.getWriter());
+    }
+
+}
