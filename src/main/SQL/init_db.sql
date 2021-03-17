e4f609b31391d9855f27f33449d8c99ddc0705ba
evaszalai <szevapianist@gmail.com>
2021-03-17 15:22:39 +0100

    feat: create carts table in codecoolshop database

--- src/main/SQL/init_db.sql
+++ src/main/SQL/init_db.sql
@@ -47,3 +47,35 @@ create unique index products_id_uindex
 alter table products
     add constraint products_pk
         primary key (id);
+
+create table items
+(
+    id serial not null,
+    product_id int not null,
+    quantity int not null,
+    constraint items_products__fk
+        foreign key (product_id) references products
+);
+
+create unique index items_id_uindex
+    on items (id);
+
+alter table items
+    add constraint items_pk
+        primary key (id);
+
+create table carts
+(
+    id serial not null,
+    user_id int not null,
+    sum_price int,
+    constraint carts_users__fk
+        foreign key (user_id) references users
+);
+
+create unique index carts_id_uindex
+    on carts (id);
+
+alter table carts
+    add constraint carts_pk
+        primary key (id);
