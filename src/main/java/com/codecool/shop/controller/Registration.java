ce72e6e20bc477cba3c6170e2d43aa186cb2da3f
kisstamas86 <kisstamas861023@gmail.com>
2021-03-18 15:00:39 +0100

    feat: html registration form

---
+++ src/main/java/com/codecool/shop/controller/Registration.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/registration"})
+public class Registration extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        context.setVariable("products", productDataStore.getAll());
+        context.setVariable("render", "ok");
+        engine.process("registration.html", context, resp.getWriter());
+    }
+
+}
